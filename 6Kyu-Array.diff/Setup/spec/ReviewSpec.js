d1f1c542402c5a39aabe9614646d533ac9800dcb
Ovy95 <jackoverton21@hotmail.com>
2021-04-20 09:53:52 +0100

    ArrayDiff Kata completed with codewars tests

--- 6Kyu-Array.diff/Setup/spec/ReviewSpec.js
+++ 6Kyu-Array.diff/Setup/spec/ReviewSpec.js
@@ -14,6 +14,14 @@ describe("ArrayDifference", function() {
       it ("Given [3,4], [3] the difference returned would be IT returns [4]",function() {
         expect(array.arrayDiff([3,4],[3])).toEqual([4]);
       })
+
+      it ("Given [3,4], [3] the difference returned would be IT returns [1,8,2]",function() {
+        expect(array.arrayDiff([1,8,2],[])).toEqual([1,8,2]);
+      })
+
+      it ("Given [1,2,3],[1,2] the difference returned would be IT returns [3]",function() {
+        expect(array.arrayDiff([1,2,3],[1,2])).toEqual([3]);
+      })
      
      
 
--- 6Kyu-Array.diff/Setup/src/review.js
+++ 6Kyu-Array.diff/Setup/src/review.js
@@ -1,19 +1,15 @@
 class ArrayDifference {
 
    arrayDiff(a,b) {
-    
-    // a  [3,4]
-    // b   [3]
 
     for (let aIndex = 0; aIndex < a.length; aIndex++) {
-        // a[aIndex]; [3,4]
-        for (let bIndex = 0; bIndex < b.length; bIndex++) {
-          // a[3]
+        
+      for (let bIndex = 0; bIndex < b.length; bIndex++) {
           if (a[aIndex] === b[bIndex]){
             a.splice(aIndex, 1); 
           }
-
         }
+
     }
 
     return a
