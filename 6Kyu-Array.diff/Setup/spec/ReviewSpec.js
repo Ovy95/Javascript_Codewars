9325ae7a3d5f1cf6710d59fe9516027474f64242
Ovy95 <jackoverton21@hotmail.com>
2021-04-20 09:49:57 +0100

    2nd passing test

--- 6Kyu-Array.diff/Setup/spec/ReviewSpec.js
+++ 6Kyu-Array.diff/Setup/spec/ReviewSpec.js
@@ -8,8 +8,13 @@ describe("ArrayDifference", function() {
 
   describe('arrayDiff', function() {
       it ("Given [], [4,5] the difference returned would be IT returns []",function() {
-        expect(array.arrayDiff([], [4,5])).toEqual([]);
+        expect(array.arrayDiff([],[4,5])).toEqual([]);
       })
+
+      it ("Given [3,4], [3] the difference returned would be IT returns [4]",function() {
+        expect(array.arrayDiff([3,4],[3])).toEqual([4]);
+      })
+     
      
 
   })
--- 6Kyu-Array.diff/Setup/src/review.js
+++ 6Kyu-Array.diff/Setup/src/review.js
@@ -2,7 +2,21 @@ class ArrayDifference {
 
    arrayDiff(a,b) {
     
-    return []
+    // a  [3,4]
+    // b   [3]
+
+    for (let aIndex = 0; aIndex < a.length; aIndex++) {
+        // a[aIndex]; [3,4]
+        for (let bIndex = 0; bIndex < b.length; bIndex++) {
+          // a[3]
+          if (a[aIndex] === b[bIndex]){
+            a.splice(aIndex, 1); 
+          }
+
+        }
+    }
+
+    return a
    
    }
 }
