793b92de7c2b7f98af0cd5d781fbd851c9d932fa
Ovy95 <jackoverton21@hotmail.com>
2021-04-23 08:07:37 +0100

    Completed Kata

--- 6Kyu/Meeting/Setup/spec/ReviewSpec.js
+++ 6Kyu/Meeting/Setup/spec/ReviewSpec.js
@@ -9,9 +9,16 @@ describe("Meeting", function() {
   describe('sortNames', function() {
 
     it ("Basic Hardcoded test returns single name in upperCase with Surname first",function() {
-      expect(meeting.sortName("Jack:Overton;")).toEqual("(OVERTON, JACK)");
+      expect(meeting.sortName("Jack:Overton")).toEqual("(OVERTON, JACK)");
     })
-    
+    // (CORWILL, WILFRED)(TORNBULL, BARNEY)
+    it ("Using two names checking added Sort method to get it in correct order",function() {
+      expect(meeting.sortName("Alexis:Wahl;John:Bell")).toEqual('(BELL, JOHN)(WAHL, ALEXIS)');
+    })
+
+    it ("Using two names checking added Sort method to get it in correct order",function() {
+      expect(meeting.sortName("Alexis:Wahl;John:Bell;Victoria:Schwarz;Abba:Dorny;Grace:Meta;Ann:Arno;Madison:STAN;Alex:Cornwell;Lewis:Kern;Megan:Stan;Alex:Korn")).toEqual('(ARNO, ANN)(BELL, JOHN)(CORNWELL, ALEX)(DORNY, ABBA)(KERN, LEWIS)(KORN, ALEX)(META, GRACE)(SCHWARZ, VICTORIA)(STAN, MADISON)(STAN, MEGAN)(WAHL, ALEXIS)');
+    })
     
 
 
--- 6Kyu/Meeting/Setup/src/review.js
+++ 6Kyu/Meeting/Setup/src/review.js
@@ -3,18 +3,28 @@ class Meeting {
    sortName(names) {
      
     var namesarray = names.toUpperCase().split(";")
+    let sortedNames = []
 
-    for (let index = 0; index < namesarray.length-1; index++) {
+    for (let index = 0; index < namesarray.length  ; index++) {
 
       let returnArray = []
       let filter = namesarray[index].toString()
-      let names = filter.split(":")
-      names[0] = (" "+names[0]+")")
-      names[1] = "("+names[1]
-      returnArray.push(names[1],names[0])
-      return returnArray.toString()
-      }
+      let split = filter.split(":")
+      split[0] = (" "+split[0]+")")
+      split[1] = "("+split[1]
 
+      returnArray.push(split[1],split[0])
+      filter = returnArray.toString()
+      sortedNames.push((filter))
+      }
+      
+      sortedNames.sort()
+      let list = ""
+         for (let index = 0; index < sortedNames.length; index++) {
+            list+=sortedNames[index]
+         }
+
+   return list
 
    }
 }
