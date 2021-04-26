22ce81f987ae76a8be1a151cff6b9082eddac8b1
Ovy95 <jackoverton21@hotmail.com>
2021-04-26 09:41:44 +0100

    passing tests for arrays that have a length of 1

--- 6Kyu/Which are in?/Setup/spec/ReviewSpec.js
+++ 6Kyu/Which are in?/Setup/spec/ReviewSpec.js
@@ -6,18 +6,16 @@ describe("inArray", function() {
     whichAre = new InArray();
   });
 
-  describe('filterArrays', function() {
+  describe('filterArrays, Arrays having a length of 1', function() {
 
-    it ("hardcoded checking setup",function() {
-      expect(whichAre.filterArrays([],[])).toEqual([]);
+    it ("returns empty array if single values don't match",function() {
+      expect(whichAre.filterArrays(["Apples"],["Pears"])).toEqual([]);
     })
-    
-    
 
-
+    it ("Returns matching single arrays",function() {
+      expect(whichAre.filterArrays(["Gym"],["Gym"])).toEqual(["Gym"]);
+    })
   })
-
- 
-
+  
 
 });
--- 6Kyu/Which are in?/Setup/src/review.js
+++ 6Kyu/Which are in?/Setup/src/review.js
@@ -1,7 +1,13 @@
 class InArray {
 
    filterArrays(array1,array2 ){
-      return []
+      let matches = []
+
+      if(array1[0]===array2[0]){
+         matches.push(array1[0])
+      }
+
+      return matches
    }
 
 }
