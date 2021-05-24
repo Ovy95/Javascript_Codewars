03dd2e8ffd9fbb0187c839756bf2258f5fee2b9c
Ovy95 <jackoverton21@hotmail.com>
2021-05-24 08:56:06 +0100

    Completed kata

--- 6Kyu/Smallest Permutation /Setup/spec/ReviewSpec.js
+++ 6Kyu/Smallest Permutation /Setup/spec/ReviewSpec.js
@@ -12,11 +12,17 @@ describe("Permutation", function() {
     it ("Given 0 // Returns 0 ",function() {
       expect(permutation.minPermutation(0)).toEqual(0);
     })
-    it ("Given 0 // Returns 0 ",function() {
+    it ("Given 10 // Returns 10 ",function() {
       expect(permutation.minPermutation(10)).toEqual(10);
     })
-    it ("Given 0 // Returns 0 ",function() {
+    it ("Given 110 // Returns 101 ",function() {
+      expect(permutation.minPermutation(110)).toEqual(101);
+    })
+    it ("Given -20 // Returns -20 ",function() {
       expect(permutation.minPermutation(-20)).toEqual(-20);
+    })
+    it ("Given 1021 // Returns 1021 ",function() {
+      expect(permutation.minPermutation(1021)).toEqual(1012);
     })
 
 
--- 6Kyu/Smallest Permutation /Setup/src/review.js
+++ 6Kyu/Smallest Permutation /Setup/src/review.js
@@ -1,8 +1,26 @@
 class Permutation {
 
-   minPermutation(num) {
-
-      return num
+   minPermutation(n) {
+      let num = n.toString().split('').sort((a,b)=>a-b).join('');
+      let count = 0;
       
+      for (let i = 0; i < num.length; i++) {
+        if (num[i] === '0') {
+          count++;
+        }
+      }
+    
+      num = num.split('');
+      if (num[0] === '-') {
+        num.splice(1, count)
+        num.splice(2, 0, '0'.repeat(count));
+      } else {
+        num.splice(0, count)
+        num.splice(1, 0, '0'.repeat(count));
+      }
+      num = num.join('');
+      
+      return Number(num);
+
    }
 }
