a0830dcbffedb8d25f58a100b369de7e38d3457e
Ovy95 <jackoverton21@hotmail.com>
2021-05-24 13:52:06 +0100

    passes 2nd test with hardcoded answer

--- 5Kyu/Pete, the Baker/Setup/spec/ReviewSpec.js
+++ 5Kyu/Pete, the Baker/Setup/spec/ReviewSpec.js
@@ -6,11 +6,14 @@ describe("Bakery", function() {
   });
 
   describe('Bakery', function() {
+
       it ("Early return called if Ingredients are not the same ",function() {
         expect(bakery.cakeMaker({flour: 500},{milk: 500})).toEqual(0);
       })
       
-      
+      it ("Returns harded 1 are objects are the same",function() {
+        expect(bakery.cakeMaker({milk: 500},{milk: 500, orangepeel: 25})).toEqual(1);
+      })
   })
   
 
--- 5Kyu/Pete, the Baker/Setup/src/review.js
+++ 5Kyu/Pete, the Baker/Setup/src/review.js
@@ -2,14 +2,42 @@ class Bakery {
 
   cakeMaker(recipe, availableIngredients) {
     const keys1 = Object.keys(recipe);
-    const keys2 = Object.keys(availableIngredients);
+    const values1 = Object.values(recipe);
 
+    const keys2 = Object.keys(availableIngredients);
+    const values2 = Object.values(availableIngredients);
+
+    
    let correctIngredients = (keys1.every(elem => keys2.includes(elem)))
     if (correctIngredients == false){
       return 0 
     }
+    let Ingredients = {Ingredient : String, amount : Number }
 
-  return 1
+
+    let IngredientsArray = []
+    for (let index = 0; index < keys2.length; index++) {
+      if (keys1[index] != undefined){
+        Ingredients = new Object();
+        Ingredients.Ingredient = keys2[index]
+        Ingredients.amount = values2[index]
+        IngredientsArray.push(Ingredients)
+      }
+    }
+
+    for (let index = 0; index < IngredientsArray.length; index++) {
+      // IngredientsArray[index]
+      let kay = IngredientsArray[index].Ingredient 
+      console.log(kay)
+      console.log(recipe[kay])
+      console.log(IngredientsArray[index].amount)
+
+      if (IngredientsArray[index].amount == recipe[kay]){
+        return 1
+      }
+
+    }
+
   }
 
 }
