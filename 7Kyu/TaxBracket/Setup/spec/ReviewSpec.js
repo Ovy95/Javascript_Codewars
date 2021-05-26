772651b97590e2b266603b45d90f3c4ee18102ff
Ovy95 <jackoverton21@hotmail.com>
2021-05-26 09:02:07 +0100

    passing first tests

--- 7Kyu/TaxBracket/Setup/spec/ReviewSpec.js
+++ 7Kyu/TaxBracket/Setup/spec/ReviewSpec.js
@@ -10,22 +10,22 @@ describe("TaxCalculator", function() {
         expect(taxCalculator.TaxBracket(0)).toEqual("0%");
       })
       it ("Personal Allowance",function() {
-        expect(taxCalculator.TaxBracket(12,570)).toEqual("0%");
+        expect(taxCalculator.TaxBracket(12570)).toEqual("0%");
       })
       it ("Basic rate",function() {
-        expect(taxCalculator.TaxBracket(12,571 )).toEqual("20%");
+        expect(taxCalculator.TaxBracket(12572)).toEqual("20%");
       })
       it ("Basic rate",function() {
-        expect(taxCalculator.TaxBracket(50,270)).toEqual("20%");
+        expect(taxCalculator.TaxBracket(50270)).toEqual("20%");
       })
       it ("Higher rate",function() {
-        expect(taxCalculator.TaxBracket(50,271)).toEqual("40%");
+        expect(taxCalculator.TaxBracket(50271)).toEqual("40%");
       })
       it ("Higher rate",function() {
-        expect(taxCalculator.TaxBracket(150,000)).toEqual("40%");
+        expect(taxCalculator.TaxBracket(150000)).toEqual("40%");
       })
       it ("Additional rate	over 150k",function() {
-        expect(taxCalculator.TaxBracket(150,001)).toEqual("45%");
+        expect(taxCalculator.TaxBracket(150001)).toEqual("45%");
       })
       
   })
--- 7Kyu/TaxBracket/Setup/src/review.js
+++ 7Kyu/TaxBracket/Setup/src/review.js
@@ -1,7 +1,25 @@
 class TaxCalculator {
 
   TaxBracket(salary){
-  return ""
+    let brackets = {PersonalAllowance:  {Max:12570,Percentage:"0%"},
+                    BasicRate:          {Max:50270,Percentage:"20%"},
+                    HigherRate:         {Max:150000,Percentage:"40%"},
+                    AdditionalRate:     {Percentage:"45%"}}
+
+    if (salary <= brackets.PersonalAllowance.Max){
+      return brackets.PersonalAllowance.Percentage
+    }
+    if ((salary <= brackets.BasicRate.Max) ){
+      return brackets.BasicRate.Percentage
+    }
+    if (salary <= brackets.HigherRate.Max ){
+      return brackets.HigherRate.Percentage
+    }
+    if (salary > brackets.HigherRate.Max){
+      return brackets.AdditionalRate.Percentage
+    }
   }
+
+  
 
 }
