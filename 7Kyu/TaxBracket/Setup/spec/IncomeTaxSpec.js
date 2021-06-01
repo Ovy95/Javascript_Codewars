2efb898704ae69c933c6e0d30bd17c8ae4c9ea39
Ovy95 <jackoverton21@hotmail.com>
2021-06-01 14:52:22 +0100

    HigherRate returning correct values

--- 7Kyu/TaxBracket/Setup/spec/IncomeTaxSpec.js
+++ 7Kyu/TaxBracket/Setup/spec/IncomeTaxSpec.js
@@ -5,10 +5,7 @@ describe("TaxCalculations", function() {
     taxCalculations = new TaxCalculations();
   });
 
-
-  
-  describe('IncomeTax works out the total amount of Income tax to be payed on salary ', function() {
-
+  describe('IncomeTax FUNCTION tests for personal allowance', function() {
     it ("Personal Allowance returns 0",function() {
       expect(taxCalculations.incomeTax(0)).toEqual(0);
     })
@@ -18,9 +15,9 @@ describe("TaxCalculations", function() {
     it ("Personal Allowance returns 0",function() {
       expect(taxCalculations.incomeTax(12570)).toEqual(0);
     })
-
-
-    it ("BasicRateMax returns 1",function() {
+  })
+  describe('BasicRateMax Function works the  works out the total amount of Income tax to be payed on salary at BASic Rate (20%) ', function() {
+    it ("BasicRateMax returns 0.2 as the it's 1 pound over the tax allowance",function() {
       expect(taxCalculations.BasicRate(12571)).toEqual(0.2);
     })
     it ("BasicRateMax returns 3486.00",function() {
@@ -29,6 +26,17 @@ describe("TaxCalculations", function() {
     it ("BasicRateMax returns 7540.00",function() {
       expect(taxCalculations.BasicRate(50270)).toEqual(7540.00);
     })
-   })
-});
+  })
+
+  describe('HigherRate Function tests checkes working out the total amount of Income tax to be payed on salary ', function() {
+  it ("Total Income tax to pay would return 3486.00",function() {
+    expect(taxCalculations.HigherRate(70000)).toEqual(15432.00);
+  })
+  it ("Total Income tax to pay would return  return 7540.40",function() {
+    expect(taxCalculations.HigherRate(50271)).toEqual(7540.40);
+  })
+  
+  });
+
+})
 
--- 7Kyu/TaxBracket/Setup/src/IncomeTax.js
+++ 7Kyu/TaxBracket/Setup/src/IncomeTax.js
@@ -2,24 +2,20 @@ class TaxCalculations {
     tax = {brackets: {
       PersonalAllowanceMax:12570,
       BasicRateMax:50270,
-      HigherRateMax:150000,
-      AdditionalRate:150001} }
+      HigherRateMax:150000} }
 
    incomeTax(salary) {
-   //  const tax = {brackets: {
-   //                    PersonalAllowanceMax:12570,
-   //                    BasicRateMax:50270,
-   //                    HigherRateMax:150000,
-   //                    AdditionalRate:150001} } 
-
       if (salary <= this.tax.brackets.PersonalAllowanceMax){
-        return 0 
+        return 0
       }
       if (salary <= this.tax.brackets.BasicRateMax){
-         taxableincome = this.twentyPercent(salary)
+         let taxableincome = this.twentyPercent(salary)
          return taxableincome
-       }
-
+      }
+      if (salary <= this.tax.brackets.HigherRateMax){
+         let taxableincome = this.HigherRate(salary)
+         return taxableincome
+      }
 
    }
 
@@ -28,6 +24,22 @@ class TaxCalculations {
       taxableincome = (taxableincome * .20);
       return taxableincome
    }
+
+   HigherRate(salary) {
+      salary = (salary - this.tax.brackets.PersonalAllowanceMax)
+      let secondBanding = (this.tax.brackets.BasicRateMax - this.tax.brackets.PersonalAllowanceMax)
+      let basicRateTax = (secondBanding * .20)
+      secondBanding = salary - secondBanding
+      let HigherRateTax = (secondBanding * .40);
+      let totalTaxableIncome = HigherRateTax + basicRateTax
+      return totalTaxableIncome
+   }
+
+   // Next steps for this are to create another Method which is Max Rate so Create the Higherrate, Also going to be running 
+   // into the edge case of not having a personal allowance on values 
+   // This is something that needs to be checked out 
+   // this will then be complete just need to do the same for national Insurance 
+   
 
 
 }
