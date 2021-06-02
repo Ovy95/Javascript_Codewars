aed3cc96578a63eab6e91cfe55877592b72dcb68
Ovy95 <jackoverton21@hotmail.com>
2021-06-02 09:54:15 +0100

    Passing Kata

--- 6Kyu/Find the odd int/Setup/spec/ReviewSpec.js
+++ 6Kyu/Find the odd int/Setup/spec/ReviewSpec.js
@@ -5,9 +5,16 @@ describe("ClassName", function() {
     oddInts = new OddInts();
   });
 
-  describe('FilterArray Function 1 value', function() {
-      it ("hardCoded test passing",function() {
+  describe('FilterArray Function', function() {
+      it ("Given 1 returns 1 ",function() {
         expect(oddInts.filterArray([1])).toEqual(1);
+      })
+      it ("Given 2,2,5 this returns 5",function() {
+        expect(oddInts.filterArray([2,2,5])).toEqual(5);
+      })
+
+      it ("Given 1,1,2,5,4,4,2 this returns 5",function() {
+        expect(oddInts.filterArray([1,1,2,5,4,4,2] )).toEqual(5);
       })
       
   })
--- 6Kyu/Find the odd int/Setup/src/review.js
+++ 6Kyu/Find the odd int/Setup/src/review.js
@@ -1,7 +1,24 @@
 class OddInts {
 
   filterArray(array){
-    return array[0]
+
+    var result = 0;
+    for(var i = 0 ; i<array.length;i++){
+    var count = 0 ;
+
+    for(var j = 0 ; j<array.length;j++){
+       if(array[i]==array[j])
+       count++;
+     }
+     if(count%2==1)
+     {
+       result = array[i];
+       break;
+     }
+  }
+  return result;
+      
+
   }
 
 }
