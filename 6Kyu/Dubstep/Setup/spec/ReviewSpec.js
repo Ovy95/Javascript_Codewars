0ff30f9edb8f910734db2f74da978fa8554263cd
Ovy95 <jackoverton21@hotmail.com>
2021-06-09 19:03:12 +0100

    Basic setup

---
+++ 6Kyu/Dubstep/Setup/spec/ReviewSpec.js
@@ -0,0 +1,19 @@
+describe("ClassName", function() {
+  var dubstep;
+
+  beforeEach (function () {
+    dubstep = new Dubstep();
+  });
+
+  describe('functionName', function() {
+      it ("",function() {
+        expect(dubstep.lyrics("")).toEqual("");
+      })
+      
+  })
+  
+
+  
+
+});
+
---
+++ 6Kyu/Dubstep/Setup/src/review.js
@@ -0,0 +1,7 @@
+class Dubstep {
+
+  lyrics(song){
+  return ""
+  }
+
+}
